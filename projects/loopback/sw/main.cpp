9f858c3647b1dedceaa65b2b58ed44e8b04d3d8b
SeMinLim <seminl1@ics.uci.edu>
2021-10-19 01:32:20 -0700

    modified main cpp file of loopback

--- projects/loopback/sw/main.cpp
+++ projects/loopback/sw/main.cpp
@@ -5,7 +5,13 @@
 #include "bdbmpcie.h"
 #include "dmasplitter.h"
 
-static const uint32_t port = 0;
+#define PORT 0
+// AuroraExt117
+// 0 means X1Y16, 1 means X1Y17
+// 2 means X1Y18, 3 means X1Y19
+// AuroraExt119
+// 4 means X1Y24, 5 means X1Y25
+// 3 means X1Y26, 6 means X1Y27
 
 double timespec_diff_sec( timespec start, timespec end ) {
 	double t = end.tv_sec - start.tv_sec;
@@ -27,21 +33,21 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
-	pcie->userWriteWord(0, port); //designate input port of AuroraExt
+	pcie->userWriteWord(0, PORT); //designate input port of AuroraExt
 
 	int readCount = 0;
 	for ( int i = 0; i < 1024; i ++ ) {
-		int d_1 = pcie->userWriteWord(0, 0xdeadbeef);
+		int d_1 = pcie->userWriteWord(1, 0xdeadbeef);
 		if ( d_1 == 0 ) {
 			printf( "Read: %x\n", pcie->userReadWord(0) );
 			readCount ++;
-			pcie->userWriteWord(0, 0xdeadbeef);
+			pcie->userWriteWord(1, 0xdeadbeef);
 		}
-		int d_2 = pcie->userWriteWord(0, 0xcafef00d);
+		int d_2 = pcie->userWriteWord(1, 0xcafef00d);
 		if ( d_2 == 0 ) {
 			printf( "Read: %x\n", pcie->userReadWord(0) );
 			readCount ++;
-			pcie->userWriteWord(0, 0xcafef00d);
+			pcie->userWriteWord(1, 0xcafef00d);
 		}
 	}
 

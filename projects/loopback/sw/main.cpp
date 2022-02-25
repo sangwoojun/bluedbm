12dd32f647c25c0d4b2ea416310717a5d7bb9d38
SeMinLim <seminl1@ics.uci.edu>
2022-02-24 20:44:06 -0800

    changed payload bits for loopback

--- projects/loopback/sw/main.cpp
+++ projects/loopback/sw/main.cpp
@@ -5,7 +5,7 @@
 #include "bdbmpcie.h"
 #include "dmasplitter.h"
 
-#define PORT 0
+#define PORT 1
 // AuroraExt117
 // 0 means X1Y16, 1 means X1Y17
 // 2 means X1Y18, 3 means X1Y19
@@ -67,17 +67,28 @@ int main(int argc, char** argv) {
 	sleep(1);
 
 	pcie->userWriteWord(0, PORT); //designate input port of AuroraExt
-	printf( "Designate input port as %d\n", PORT );
+	printf( "Designate input port as %d\n\n", PORT );
 	fflush( stdout );
-	
-	for ( int i = 0; i < 3; i ++ ) {
-		pcie->userWriteWord(4, 0xdeadbeef);
-		pcie->userWriteWord(4, 0xcafef00d);
-	}
-	printf( "Send Done! (deadbeef & cafef00d)\n\n" );
+	sleep(2);
+
+	pcie->userWriteWord(4, 0xacedc0de);
+	pcie->userWriteWord(4, 0xbabeface);
+	printf( "Send the first payload done! (acedc0de & babeface)\n\n" );	
 	fflush( stdout );
 	sleep(1);
-	
+
+	pcie->userWriteWord(4, 0xdeadbeef);
+	pcie->userWriteWord(4, 0xcafef00d);
+	printf( "Send the second payload done! (deadbeef & cafef00d)\n\n" );
+	fflush( stdout );
+	sleep(1);
+
+	pcie->userWriteWord(4, 0xfeedc0c0);
+	pcie->userWriteWord(4, 0xb00bdace);
+	printf( "Send the third payload done! (feedc0c0 & b00bdace)\n\n" );
+	fflush( stdout );
+	sleep(1);
+
 	resCnt = 16;
 	for ( int i = 0; i < 4; i ++ ) {
 		printf( "Quad 117\n" );
@@ -85,27 +96,43 @@ int main(int argc, char** argv) {
 		fflush(stdout);
 		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
 		fflush(stdout);
+		printf( "X1Y%d received(1/2): %x\n", resCnt, pcie->userReadWord(i*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(1/2): %x\n", resCnt, pcie->userReadWord(i*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(1/2): %x\n", resCnt, pcie->userReadWord(i*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(1/2): %x\n", resCnt, pcie->userReadWord(i*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
+		fflush(stdout);
 		resCnt ++;
-		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
-		fflush(stdout);
-		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
-		fflush(stdout);
-		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
-		fflush(stdout);
-		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
-		fflush(stdout);
-		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
-		fflush(stdout);
-		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
-		fflush(stdout);
-		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
-		fflush(stdout);
-		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord(i*4) );
-		fflush(stdout);
 	}
 	resCnt = 24;
 	for ( int i = 0; i < 4; i ++ ) {
 		printf( "Quad 119\n" );
+		printf( "X1Y%d received(1/2): %x\n", resCnt, pcie->userReadWord((4+i)*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord((4+i)*4 ));
+		fflush(stdout);
+		printf( "X1Y%d received(1/2): %x\n", resCnt, pcie->userReadWord((4+i)*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord((4+i)*4 ));
+		fflush(stdout);
+		printf( "X1Y%d received(1/2): %x\n", resCnt, pcie->userReadWord((4+i)*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord((4+i)*4 ));
+		fflush(stdout);
+		printf( "X1Y%d received(1/2): %x\n", resCnt, pcie->userReadWord((4+i)*4) );
+		fflush(stdout);
+		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord((4+i)*4 ));
+		fflush(stdout);
 		printf( "X1Y%d received(1/2): %x\n", resCnt, pcie->userReadWord((4+i)*4) );
 		fflush(stdout);
 		printf( "X1Y%d received(2/2): %x\n", resCnt, pcie->userReadWord((4+i)*4 ));

939bd4fe7394fc890b4089a6d6344de65a6d1544
SeMinLim <seminl1@ics.uci.edu>
2022-02-24 21:45:17 -0800

    added program sh files

---
+++ distribute/program1st.sh
@@ -0,0 +1,4 @@
+vivado -mode batch -source ~/bluedbm/distribute/program1st.tcl -nolog -nojournal
+
+sleep 2
+bsrescan
---
+++ distribute/program2nd.sh
@@ -0,0 +1,4 @@
+vivado -mode batch -source ~/bluedbm/distribute/program2nd.tcl -nolog -nojournal
+
+sleep 2
+bsrescan
